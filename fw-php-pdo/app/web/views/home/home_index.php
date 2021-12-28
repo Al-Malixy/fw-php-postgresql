0aba0f3884ee45a513e3243cc01eccd665e923b4
Muhamad Maulana Malik Ibrahim <almalixy@gmail.com>
2021-12-28 07:28:18 +0700

    Delete fw-php-pdo directory

--- fw-php-pdo/app/web/views/home/home_index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data_array = $data['pegawai']->fetchAll(PDO::FETCH_ASSOC);
-echo "<ul>";
-foreach ($data_array as $row) {
-    //print("<pre>" . print_r($row, true) . "</pre>");
-    echo "<li>NIY: " . $row['niy'] . ", Nama: " . $row['nama'] . ", Email: " . $row['email'] . "</li>";
-}
-echo "</ul>";
--- fw-php-pdo/core/Init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "Config.php";
-require_once "Loader.php";
-require_once "Database.php";
-require_once "Middleware.php";
-require_once "Controller.php";
-require_once "Model.php";
-require_once "App.php";
